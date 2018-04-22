486517a0339a7d28bba4d24066749952a5030766
ChimengSoso <meng741258@gmail.com>
2018-04-22 23:56:25 +0700

    add new image and doing debug code

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 import time
-import threading
+
 from random import *
+from math import *
 
 pygame.init()
 
@@ -79,6 +80,9 @@ def draw_line(x1, y1, x2, y2, color = COLOR_WHITE, size_line = 2):
 def draw_rect(pos_x, pos_y, widht_rect, height_rect, color = COLOR_WHITE):
     pygame.draw.rect(gameDisplay, color, [pos_x, pos_y, widht_rect, height_rect])
 
+def draw_circle(x, y, redius, color = COLOR_WHITE) :
+    pygame.draw.circle(gameDisplay, color, (x, y), int(redius))
+
 def push_img(img, x, y): # push image to screen in position (x, y)
     gameDisplay.blit(img, (x, y))
 
@@ -105,14 +109,13 @@ def player(x, y, state = 'live'):
 
 def traffic_LTR(cars) :
     n = len(cars)
-    print(cars)
     remove_car = set()
     for i in range(n):
         car = cars[i]
         car_x = car[0]
         car_y = car[1]
         speed_car = car[2]
-        draw_rect(car_x, car_y + 5, 40, 22, COLOR_RED)
+        draw_circle(car_x, car_y+15, 15)
         cars[i][0] += speed_car      # CHANGE SPEED OF cars[i]
         if car_x >= WIDTH :
             remove_car.add(i)
@@ -127,14 +130,13 @@ def traffic_LTR(cars) :
 
 def traffic_RTL(cars) :
     n = len(cars)
-    print(cars)
     remove_car = set()
     for i in range(n):
         car = cars[i]
         car_x = car[0]
         car_y = car[1]
         speed_car = car[2]
-        draw_rect(car_x, car_y + 5, 40, 22, COLOR_RED)
+        draw_circle(car_x, car_y+15, 15, COLOR_RED)
         cars[i][0] += speed_car      # CHANGE SPEED OF cars[i]
         if car_x < 0 :
             remove_car.add(i)
@@ -147,22 +149,35 @@ def traffic_RTL(cars) :
     cars.clear()
     cars.extend(car_live)
 
-def overlab(img, x_img, y_img, obj) :
+def overlab(img, x_img, y_img, obj, type_obj = 'rect') :
     img_x = x_img
     img_y = y_img
     img_width = img.get_width()
     img_hieght = img.get_height()
     obj_x = obj[0]
     obj_y = obj[1]
-    obj_width = obj[2]
-    obj_hieght = obj[3]
-    x_overlab = (img_x <= obj_x + obj_width and img_x + img_width >= obj_x)
-    y_overlab = (img_y <= obj_y + obj_hieght and img_y + img_hieght >= obj_y)
-    return x_overlab and y_overlab;
+    if type_obj == 'rect' :    
+        obj_width = obj[2]
+        obj_hieght = obj[3]
+        x_overlab = (img_x <= obj_x + obj_width and img_x + img_width >= obj_x)
+        y_overlab = (img_y <= obj_y + obj_hieght and img_y + img_hieght >= obj_y)
+        return x_overlab and y_overlab
+    elif type_obj == 'circle':
+        obj_r = 15
+        img_x_list = [img_x]
+        img_y_list = [img_y]
+        for i in range(1):
+            dx = img_x_list[i] - obj_x
+            dy = img_y_list[i] - obj_y
+            r  = obj_r + 10
+            # print(sqrt(dx*dx + dy*dy), r)
+            if dx*dx + dy*dy <= r*r:
+                return True
+        return False
+    return False
 
-def create_runway(hieght_of_runway, type_of_runway = None) :
+def create_runway(hieght_of_runway, type_of_runway = None, n_car = 1) :
     cars_runway = list()
-    n_car = randint(1, 5)
     speed_car = randint(1, 5)
 
     if type_of_runway == 'left_to_rigth':
@@ -180,7 +195,7 @@ def create_runway(hieght_of_runway, type_of_runway = None) :
         cars_runway.append([start_x_car, hieght_of_runway, speed_car])
 
         for i in range(1, n_car):
-            speed_car = randint(-5, -cars_runway[i-1][2])
+            speed_car = randint(-5, cars_runway[i-1][2])
             start_x_car = cars_runway[i-1][0] + 50
             cars_runway.append([start_x_car, hieght_of_runway, speed_car])
 
@@ -213,8 +228,8 @@ def game_loop():
     GROUND = (GROUND_X, GROUND_Y, GROUND_WIDTH, GROUND_HIEGHT)
 
     #SET CAR INFO
-    car_runway_LTR = create_runway(POS_Y[1], 'left_to_rigth') + create_runway(POS_Y[3], 'left_to_rigth')
-    car_runway_RTL = create_runway(POS_Y[2], 'right_to_left') + create_runway(POS_Y[4], 'right_to_left')
+    car_runway_LTR = create_runway(POS_Y[1], 'left_to_rigth') #+ create_runway(POS_Y[3], 'left_to_rigth')
+    # car_runway_RTL = create_runway(POS_Y[2], 'right_to_left') + create_runway(POS_Y[4], 'right_to_left')
 
     while not GAME_OVER:
         # =============== EVENT PROCESSING ===================== #
@@ -242,8 +257,12 @@ def game_loop():
                     pass
 
         # ===================== LOGIC GAME ======================= #
+        # if len(car_runway_RTL) < 5:
+            # car_runway_RTL.extend(create_runway(POS_Y[2*randint(1, 2)], 'right_to_left'))
 
-        
+        if len(car_runway_LTR) < 1:
+            # car_runway_LTR.extend(create_runway(POS_Y[2*randint(1, 2)-1], 'left_to_rigth'))
+            car_runway_LTR.extend(create_runway(POS_Y[1], 'left_to_rigth'))
 
         cur_x = max(cur_x, LEFT_BOUND)  # LIMIT BOUND OF SIDE LEFT
         cur_x = min(cur_x, RIGHT_BOUND) # LINIT BOUND OF SIDE RIGHT
@@ -258,6 +277,14 @@ def game_loop():
         if in_ground == False:
             ply_stete = 'drowned'
 
+        n_cars = len(car_runway_LTR)
+        for i in range(n_cars):
+            if overlab(IMG_PLY, ply_x, ply_y, car_runway_LTR[i], 'circle'):
+                print(car_runway_LTR[i])
+                ply_x += car_runway_LTR[i][2]
+                cur_x += car_runway_LTR[i][2]
+                # print('hit ', ply_x)
+
         """ ============ DISPLAY OF GAME ============= """
         fill_scr(COLOR_BLACK)
         
@@ -266,7 +293,7 @@ def game_loop():
         # draw_gird()
         player(ply_x, ply_y, ply_stete)
         traffic_LTR(car_runway_LTR)
-        traffic_RTL(car_runway_RTL)
+        # traffic_RTL(car_runway_RTL)
 
         draw_rect(0, 70, 25, 495, COLOR_BLACK)    # DRAW BOUND LEFT
         draw_rect(525, 70, 25, 495, COLOR_BLACK)  # DRAW BOUND RIGHT
